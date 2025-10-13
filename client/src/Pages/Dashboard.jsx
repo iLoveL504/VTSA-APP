2d3c2acb655909cb8d82c54e281dd79a1fe3bf18
Reyal <chingcuangco7904@gmail.com>
2025-10-13 15:34:44 +0800

    10/13/25

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStoreState } from 'easy-peasy';
 import MyGanttComponent from "../outComponent/GANTT_CHART/GanttChart.jsx";
